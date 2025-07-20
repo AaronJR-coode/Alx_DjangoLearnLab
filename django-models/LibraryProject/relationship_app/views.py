103a21ca9a3d5255e09aa1bd0f36d1296d8ecf79
AaronJR-coode <khombeaaronjunior@gmail.com>
2025-07-20 12:18:51 +0200

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.views.generic import DetailView
+from .models import Book, Library
 
 # Create your views here.
+def book_list_view(request):
+    books = Book.objects.select_related('author').all()
+    return render(request, 'relationship_app/book_list.html', {
+        'books': books
+    })
+
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'relationship_app/library_detail.html'
+    context_object_name = 'library'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['books'] = self.object.books.select_related('author').all()
+        return context
