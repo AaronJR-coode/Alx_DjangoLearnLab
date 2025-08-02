e51768798e73537350dfc85f1dae46463c316c79
AaronJR-coode <khombeaaronjunior@gmail.com>
2025-08-02 21:35:30 +0200

    Update urls.py

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -9,6 +9,7 @@ router.register(r'books_all', BookViewSet, basename='book_all')
 urlpatterns = [
     path('books/', BookList.as_view(), name='book-list'),
 
-    path('', include('api.urls'))
+    path('', include(router.urls))
 ]
+
 
